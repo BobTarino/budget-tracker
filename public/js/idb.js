ee28066c52a044aed386349217f0b0a3773cf3f3
Bobby T <bob.tarino@gmail.com>
2021-07-31 10:50:48 -0500

    open new transaction w/ db with read and write permissions

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,6 +6,33 @@ const request = indexedDB.open('budget_tracker', 1);
 request.onupgradeneeded = function(event) {
     // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('new_pizza', { autoIncrement: true });
+    // create an object store (table) called `new_transaction', set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_transaction', { autoIncrement: true });
 };
+// upon a successful 
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+    db = event.target.result;
+  
+    // check if app is online, if yes run uploadTransaction() function to send all local db data to api
+    if (navigator.onLine) {
+      // uploadTransaction();
+    }
+  };
+  
+request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+};
+
+// This function will be executed if we attempt to submit a new Transaction and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction ((not to be confused with transaction model)) with the database with read and write permissions 
+    const dbTransaction = db.dbTransaction(['new_Transaction'], 'readwrite');
+  
+    // access the object store for `new_Transaction`
+    const TransactionObjectStore = dbTransaction.objectStore('new_Transaction');
+  
+    // add record to your store with add method
+    TransactionObjectStore.add(record);
+}
