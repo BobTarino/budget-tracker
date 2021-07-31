b701a693b514cc77f2dbe4775fdc1a46ccf182df
Bobby T <bob.tarino@gmail.com>
2021-07-31 15:04:11 -0500

    activated service worker & added event listener to intercept fetch requests

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,3 +16,35 @@ self.addEventListener('install', function (e) {
       })
     )
 })
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+      caches.keys().then(function(keyList) {
+        let cacheKeeplist = keyList.filter(function(key) {
+          return key.indexOf(APP_PREFIX);
+        });
+        cacheKeeplist.push(CACHE_NAME);
+        // resolves once all old versions of the cache have been deleted
+        return Promise.all(
+          keyList.map(function(key, i) {
+            //   will only return value of -1 if item is not found in keeplist
+            if (cacheKeeplist.indexOf(key) === -1) {
+              console.log('deleting cache : ' + keyList[i]);
+              return caches.delete(keyList[i]);
+            }
+          })
+        );
+      })
+    );
+});
+
+// application knows to retrieve info from cache
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    // intercept fetch request
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        return request || fetch(e.request)
+      })
+    )
+  })
